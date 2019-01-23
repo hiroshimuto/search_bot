35dc3a7b03baf5a88f7fd4ac57bd6fb5b4d67a7c
hiroshimuto <hiroshi.github@gmail.com>
2019-01-23 18:51:27 +0900

    debug

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -20,6 +20,9 @@ class LinebotController < ApplicationController
     search_box = driver.find_element(:name, 'q') # 検索欄
     # 入力欄に'送信されたメッセージ'を入力し、エンターを実行
     search_box.send_keys(search_word, :enter)
+    #ニュースボタンが表示されるまで待機
+    wait = Selenium::WebDriver::Wait.new(:timeout => 2)
+    wait.until {driver.find_element(:link_text,'ニュース').displayed?}
     #ニュースボタンを変数に代入
     news_btn = driver.find_element(:link_text,'ニュース')
     #ニュースボタンをクリック
